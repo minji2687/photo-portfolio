02b81743c37ba148195e56dbb7ad6214939c9e71
Jo Minji <mingi2687@gamil.com>
2021-11-24 20:12:03 +0900

    내정보 페이지 데이터 바인딩

--- js/script.js
+++ js/script.js
@@ -8,3 +8,34 @@ function setMenu(_menu) {
 
   document.querySelector("main").className = _menu;
 }
+
+function setDescLength() {
+  let descLengthSpan = document.querySelector("span.descLength");
+  descLengthSpan.innerHTML =
+    document.querySelector("input.description").value.length + "/20";
+}
+
+function showMyInfo() {
+  document.querySelector("#myInfoId").innerHTML = my_info.id;
+  document.querySelector("#myInfoUserName ").innerHTML = my_info.user_name;
+  document.querySelector("#ip-intro ").value = my_info.introduction;
+  document.querySelector("#sp-intro ").innerHTML = my_info.introduction;
+  document.querySelector(
+    "#myinfo input[type=radio][value=" + my_info.as + "]"
+  ).checked = true;
+
+  document
+    .querySelectorAll("#myinfo input[type=checkbox]")
+    .forEach(function (checkbox) {
+      checkbox.checked = false;
+    });
+  my_info.interest.forEach(function (interest) {
+    document.querySelector(
+      "#myinfo input[type=checkbox][value=" + interest + "]"
+    ).checked = true;
+  });
+}
+
+function init() {
+  showMyInfo();
+}
